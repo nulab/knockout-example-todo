6918139e58469fa4fd9a9e428c58d460b7881619
eguchi <eguchi@nulab.co.jp>
2013-07-01 11:11:36 +0900

    second commit

--- todo.js
+++ todo.js
@@ -2,16 +2,34 @@
     function ViewModel() {
         var self = this;
 
-        self.todoList = ko.observableArray();
+        self.todoList = ko.observableArray([
+                                               {summary: 'test', done: ko.observable(false)}
+                                           ]);
         self.todoSummary = ko.observable('');
         self.addTodo = function () {
-            var todo = {
-                summary: self.todoSummary(),
-                done: ko.observable(false)
-            };
+            if (self.canAddTodo()) {
+                var todo = {
+                    summary: self.todoSummary(),
+                    done: ko.observable(false)
+                };
 
-            self.todoList.push(todo);
-            self.todoSummary('');
+                self.todoList.push(todo);
+                self.todoSummary('');
+            }
+        };
+
+        self.canAddTodo = ko.computed(function () {
+            return self.todoSummary().length > 0;
+        });
+
+        self.deleteTodo = function (todo) {
+            self.todoList.remove(todo);
+        };
+
+        self.archive = function () {
+            self.todoList.remove(function (todo) {
+                return todo.done();
+            });
         };
     }
 
